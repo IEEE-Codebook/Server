44f1f37abd9576265087c4e74f018028a2465026
Radhika Chhabra <7radhikachhabra7@gmail.com>
2023-01-17 18:25:22 +0530

    first commit

--- middleware/requireLogin.js
+++ middleware/requireLogin.js
@@ -5,9 +5,9 @@ const User = mongoose.model("User")
 
 module.exports = (req, res, next) => {
     const {authorization} = req.headers
-    // if(!authorization) {
-    //     return res.status(401).json({error:"you must log in"})
-    // }
+    if(!authorization) {
+        return res.status(401).json({error:"you must log in"})
+    }
     const token = authorization.replace("Bearer ", "")
     //const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2M2Njg0MjI4NGQ1YzhiOTkyMDlkOWUiLCJpYXQiOjE2NzM5NTI1Mjd9.FFfKN2d-YAVQPuj8ypRkRpRoCY-92_igM9X-ZVpItVc"
     jwt.verify(token, JWT_SECRET, (err, payload) => {
