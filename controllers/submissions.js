e7c865b25d312fc9291637dcdf84c833c64b6715
Dhruvil-Lakhtaria <lakhtariadhruvil123@gmail.com>
2023-03-29 19:49:35 +0530

    atcoder added

--- controllers/submissions.js
+++ controllers/submissions.js
@@ -9,20 +9,29 @@ const mySubmission = async (req, res) => {
 const friendSubmission = async (req, res) => {
   const following = req.user.following;
   var subs = [];
-
+  var atSubs = [];
   for (var i = 0; i < following.length; i++) {
     const u = await user.findOne({ _id: following[i] });
-    const URL =
-      "https://codeforces.com/api/user.status?handle=" +
-      u.codeforces +
-      "&from=1&count=100";
-    const submits = await axios.get(URL);
-    subs.push(u.codeforces);
-    subs.push(submits.data.result);
-    // console.log(subs);
+    if (u.codeforces) {
+      const URL =
+        "https://codeforces.com/api/user.status?handle=" +
+        u.codeforces +
+        "&from=1&count=100";
+      const submits = await axios.get(URL);
+      subs.push(u.codeforces);
+      subs.push(submits.data.result);
+    }
+    if (u.atcoder) {
+      const atCoder_URL =
+        "https://kenkoooo.com/atcoder/atcoder-api/v3/user/submissions?user=" +
+        u.atcoder +
+        "&from_second=0";
+      const atSubmit = await axios.get(atCoder_URL);
+      atSubs.push(u.atcoder);
+      atSubs.push(atSubmit.data);
+    }
   }
-  // console.log(subs);
-  res.status(200).json({ handles: subs });
+  res.status(200).json({ handles: subs, atcoder: atSubs });
 };
 
 const getSubmission = async (req, res) => {};
